691394cb73df82c1b423262ae69c54eb9005eaf4
Mark Hollands <M.Hollands.1@warwick.ac.uk>
2020-09-16 18:00:21 +0100

    Updated docstring

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 """
-Module for working with astropyhsical spectra
+Module for interpolating white dwarf masses, radii and various other important
+stellar parameters. The code currently uses the Bedard et al. (2020) grid of
+evolutionary models, by default. Though it is still possible to use the older
+Fontaine et al. (2001) grid if required.
 """
 __author__ = "Mark Hollands"
 __email__ = "M.Hollands.1@warwick.ac.uk"
