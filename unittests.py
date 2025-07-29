f26e7fe4a3182ecb828b5e70594a0609c833feb3
Mark Hollands <phsvjw@vala.astro.warwick.ac.uk>
2025-07-29 10:36:42 +0100

    Merged unit tests with parametrize

--- unittests.py
+++ unittests.py
@@ -5,87 +5,69 @@ from mh.MR_relation import *
 
 
 class Test_inputs:
-    class Test_valid_inputs:
-        def test_input1_type(self):
-            with pytest.raises(TypeError):
-                M_from_Teff_logg("test", 8.0, thickness="thick")
+    @pytest.mark.parametrize(
+    "x, y, thickness, err", [
+        ("abc", 8.00, "thick", TypeError),
+        (10000, "ab", "thick", TypeError),
+        (10000, 8.00, 1234567, TypeError),
+        (10000, 8.00, "wrong", ValueError),
+    ])
+    def test_input_exceptions(self, x, y, thickness, err):
+        with pytest.raises(err):
+            M_from_Teff_logg(x, y, thickness=thickness)
 
-        def test_input2_type(self):
-            with pytest.raises(TypeError):
-                M_from_Teff_logg(10000, "test", thickness="thick")
-
-        def test_input3_type(self):
-            with pytest.raises(TypeError):
-                M_from_Teff_logg(10000, 8.0, thickness=5)
-
-        def test_input3_value(self):
-            with pytest.raises(ValueError):
-                M_from_Teff_logg(10000, 8.0, thickness="wrong")
-
-    class Test_floats_vs_array:
-        def test_float_float(self):
-            logg = logg_from_M_R(0.6, 0.013)
-            assert isinstance(logg, np.float64)
-
-        def test_array_float(self):
-            M = np.random.normal(0.6, 0.01, size=100)
-            logg = logg_from_M_R(M, 0.013)
-            assert isinstance(logg, np.ndarray)
-
-        def test_float_array(self):
-            R = np.random.normal(0.013, 0.001, size=100)
-            logg = logg_from_M_R(0.6, R)
-            assert isinstance(logg, np.ndarray)
-
-        def test_array_array(self):
-            M = np.random.normal(0.6, 0.01, size=100)
-            R = np.random.normal(0.013, 0.001, size=100)
-            logg = logg_from_M_R(M, R)
-            assert isinstance(logg, np.ndarray)
+    @pytest.mark.parametrize(
+    "x, y, expected_type", [
+        (0.6, 0.013, np.float64),
+        (0.6*np.ones(100), 0.013, np.ndarray),
+        (0.6, 0.013*np.ones(100), np.ndarray),
+        (0.6*np.ones(100), 0.013*np.ones(100), np.ndarray),
+    ])
+    def test_floats_vs_array(self, x, y, expected_type):
+        logg = logg_from_M_R(x, y)
+        assert isinstance(logg, expected_type)
 
     class Test_units:
         def test_no_units(self):
             logg = logg_from_M_R(0.6, 0.013)
             assert not isinstance(logg, u.Quantity)
 
-        def test_units_on_1(self):
-            logg = logg_from_M_R(0.6 * u.Msun, 0.013)
+        @pytest.mark.parametrize(
+        "x, y", [
+            (0.6*u.Msun, 0.013),
+            (0.6, 0.013*u.Rsun),
+            (0.6*u.Msun, 0.013*u.Rsun),
+        ])
+        def test_with_units(self, x, y):
+            logg = logg_from_M_R(x, y)
             assert isinstance(logg, u.Quantity)
             assert logg.unit == u.dex(u.cm / u.s**2)
 
-        def test_units_on_2(self):
-            logg = logg_from_M_R(0.6, 0.013 * u.Rsun)
-            assert isinstance(logg, u.Quantity)
-            assert logg.unit == u.dex(u.cm / u.s**2)
-
-        def test_units_on_both(self):
-            logg = logg_from_M_R(0.6 * u.Msun, 0.013 * u.Rsun)
-            assert isinstance(logg, u.Quantity)
-            assert logg.unit == u.dex(u.cm / u.s**2)
-
-        def test_wrong_units_on_1(self):
+        @pytest.mark.parametrize(
+        "x, y", [
+            (0.6*u.K, 0.013),
+            (0.6, 0.013*u.erg),
+            (0.6*u.K, 0.013*u.erg),
+        ])
+        def test_wrong_units(self, x, y):
             with pytest.raises(u.UnitsError):
-                logg = logg_from_M_R(0.6 * u.K, 0.013)
-
-        def test_wrong_units_on_2(self):
-            with pytest.raises(u.UnitsError):
-                logg = logg_from_M_R(0.6, 0.013 * u.erg)
+                logg = logg_from_M_R(x, y)
 
 
 class Test_model_grids:
-    def test_switch_grid(self):
-        # changing model grid should give different results
-        tau1 = tau_from_Teff_M(10000, 0.6, "thick")
-        MR_grid.set_grid("Fontaine01")
-        tau2 = tau_from_Teff_M(10000, 0.6, "thick")
-        assert tau1 != tau2
+    @pytest.mark.parametrize(
+        "value",
+        [
+            "Bedard20",
+            "Fontaine01",
+            "Camisassa25",
+            "Althaus13ELM",
+        ],
+    )
+    def test_valid_grids(self, value):
+        MR_grid.set_grid(value)
 
-    def test_all_grids(self):
-        MR_grid.set_grid("Bedard20")
-        MR_grid.set_grid("Fontaine01")
-        MR_grid.set_grid("Camisassa25")
-        MR_grid.set_grid("Althaus13ELM")
-
+    def test_invalid_grid(self):
         with pytest.raises(ValueError):
             MR_grid.set_grid("Something else")
 
@@ -93,3 +75,11 @@ class Test_model_grids:
         MR_grid.set_grid("Althaus13ELM")
         with pytest.raises(ValueError):
             tau2 = tau_from_Teff_M(10000, 0.6, "thin")
+
+    def test_switch_grid(self):
+        # changing model grid should give different results
+        MR_grid.set_grid("Bedard20")
+        tau1 = tau_from_Teff_M(10000, 0.6, "thick")
+        MR_grid.set_grid("Fontaine01")
+        tau2 = tau_from_Teff_M(10000, 0.6, "thick")
+        assert tau1 != tau2
